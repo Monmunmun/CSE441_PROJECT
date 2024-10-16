6daf06d7cf45791df572743440996c803ed66f99
Monmunmun <imubslayer4@gmail.com>
2024-10-16 21:54:24 +0700

    BookUser

---
+++ CSE_PROJECT/app/src/main/java/com/example/cse_project/Books.java
@@ -0,0 +1,68 @@
+package com.example.cse_project;
+
+public class Books {
+    private int id;
+    private String title;
+    private String author;
+    private double price;
+    private String imageUrl;
+
+    public Books(int id, String title, String author, double price, String imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Books{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
